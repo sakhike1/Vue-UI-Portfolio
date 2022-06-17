57627df8d222cbe7b33f07fb2cef1b5eac467fcd
let firstName = "Sakhile <51701132+sakhike1@users.noreply.github.com>
2022-06-17 10:02:29 +0200

    Update AboutComponentTwo.vue

--- src/components/About/AboutComponentTwo.vue
+++ src/components/About/AboutComponentTwo.vue
@@ -1,8 +1,8 @@
-<template>
-  <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+<template class="bg-black">
+  <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full  md:px-24 lg:px-8 lg:py-20 bg-black">
     <div class="grid gap-5 row-gap-10 lg:grid-cols-2">
-      <div class="flex flex-col justify-center">
-        <div class="max-w-xl mb-6">
+      <div class="flex flex-col justify-center ">
+        <div class="max-w-xl mb-6 ">
           <h2
             class="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-indigo-600 sm:text-4xl sm:leading-none">
             Check out, some of my <br class="hidden md:block" />
@@ -11,7 +11,7 @@
               <div class="absolute inset-x-0 bottom-0 h-3 transform -skew-x-12 bg-teal-accent-400"></div>
             </span>
           </h2>
-          <p class="text-base text-gray-700 max-w-xl mx-auto mt-8 leading-relaxed md:text-lg">
+          <p class="text-base text-neutral-600 max-w-xl mx-auto mt-8 leading-relaxed md:text-lg">
             Vue.js, Angular, Javascript, Tailwind, HTML,laravel, CSS, SCSS, UXD, Figma,
             Git, Vscode, macOS, windows, Vercel, Netlify, firebase,Vueex, bootstrap,Vuetify,Next.js
           </p>
