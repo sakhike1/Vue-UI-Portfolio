c8a77226ee7f17cd2f4e0980ebecf55f26d5f9e5
Sakhile Simelane <51701132+sakhike1@users.noreply.github.com>
2022-06-29 20:02:39 +0200

    Update HomeComponentOne.vue

--- src/components/Home/HomeComponentOne.vue
+++ src/components/Home/HomeComponentOne.vue
@@ -10,7 +10,7 @@
 
         <div class="lg:py-24">
           <h2 class="text-3xl text-white font-bold sm:text-4xl">
-            Frontend developer
+            Front-end developer
           </h2>
 
           <p class="mt-4 text-slate-500">
