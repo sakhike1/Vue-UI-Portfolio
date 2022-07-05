c2fa2d78a71bd386e86cb62bba90d0bfc075adc7
Sakhile Simelane <51701132+sakhike1@users.noreply.github.com>
2022-07-06 00:03:18 +0200

    Update FooterComponent.vue

--- src/components/Shared/FooterComponent.vue
+++ src/components/Shared/FooterComponent.vue
@@ -1,35 +1,43 @@
 <template>
   <footer
-    class="text-white bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 hover:from-pink-500 hover:to-yellow-500">
+    class="text-white bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 hover:from-pink-500 hover:to-yellow-500"
+  >
     <div class="max-w-screen-xl px-4 pt-32 pb-12 mx-auto sm:px-6 lg:px-8">
       <div class="max-w-3xl mx-auto text-center">
         <p class="text-4xl font-extrabold sm:text-6xl">
           Do you have a project in mind
         </p>
 
-        <a class="inline-block px-12 py-3 mt-8 text-sm font-medium border rounded-full border-white/25 hover:bg-white hover:text-yellow-500"
-          href="/#Contact">
+        <a
+          class="inline-block px-12 py-4 mt-8 text-sm font-medium border rounded-full border-white/25 hover:bg-white hover:text-yellow-500"
+          href="/#Contact"
+        >
           Contact me
         </a>
       </div>
 
-      <div class="pt-12 mt-32 border-t border-white/25 sm:items-center sm:justify-between sm:flex">
-
-
+      <div
+        class="pt-12 mt-32 border-t border-white/25 sm:items-center sm:justify-between sm:flex"
+      >
         <div class="flex justify-center mt-6 space-x-6 sm:justify-end sm:mt-0">
-          <a class="p-2 border rounded-full border-white/25 hover:opacity-75" href="" target="_blank" rel="noreferrer">
+          <a
+            class="p-2 border rounded-full border-white/25 hover:opacity-75"
+            href=""
+            target="_blank"
+            rel="noreferrer"
+          >
             <span class="sr-only"> Linkedin </span>
 
-            <svg class="w-6 h-6 text-blue fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+            <svg
+              class="w-6 h-6 text-blue fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+            >
               <path
-                d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z">
-              </path>
+                d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"
+              ></path>
             </svg>
           </a>
-
-
-
-
         </div>
       </div>
     </div>
@@ -40,5 +48,4 @@
 export default {};
 </script>
 
-<style>
-</style>
+<style></style>
