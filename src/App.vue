3fcfe6250f6212a0fa07845226ceadd26b5b2d00
Sakhile Simelane <51701132+sakhike1@users.noreply.github.com>
2022-07-08 11:52:45 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,59 @@
 <template>
-  <div class="bg-gradient-to-r from-purple-400 to-yellow-400">
-    <nav
-      class="container px-6 py-12 mx-auto md:flex md:justify-between md:items-center"
-    >
-      <div class="flex items-center justify-between">
-        <router-link
-          to="/"
-          class="font-bold text-white md:text-2xl hover:text-orange-400"
-          >Sakhile's Portfolio
-        </router-link>
-        <!-- Mobile menu button -->
-        <div @click="toggleNav" class="flex md:hidden">
-          <button
-            type="button"
-            class="text-gray-100 hover:text-gray-400 focus:outline-none focus:text-gray-400"
-          >
-            <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
-              <path
-                fill-rule="evenodd"
-                d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-              ></path>
-            </svg>
-          </button>
-        </div>
-      </div>
-
-      <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
-      <ul
-        :class="showMenu ? 'flex' : 'hidden'"
-        class="flex-col mt-8 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0"
+  <router-view v-slot="{ Component }">
+    <div class="bg-gradient-to-r from-purple-400 to-yellow-400">
+      <nav
+        class="container px-6 py-12 mx-auto md:flex md:justify-between md:items-center"
       >
-        <li><router-link class="text-gray-100" to="/">Home</router-link></li>
-        <li
-          ><router-link class="text-gray-100" to="/about"
-            >About me</router-link
-          ></li
+        <div class="flex items-center justify-between">
+          <router-link
+            to="/"
+            class="font-bold text-white md:text-2xl hover:text-orange-400"
+            >Sakhile's Portfolio
+          </router-link>
+          <!-- Mobile menu button -->
+          <div @click="toggleNav" class="flex md:hidden">
+            <button
+              type="button"
+              class="text-gray-100 hover:text-gray-400 focus:outline-none focus:text-gray-400"
+            >
+              <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
+                <path
+                  fill-rule="evenodd"
+                  d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+                ></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
+        <ul
+          :class="showMenu ? 'flex' : 'hidden'"
+          class="flex-col mt-8 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0"
         >
-        <li
-          ><router-link class="text-gray-100" to="/projects"
-            >Projects</router-link
-          ></li
-        >
-        <li
-          ><router-link class="text-gray-100" to="/Contact"
-            >Contact Me</router-link
-          ></li
-        >
-      </ul>
-    </nav>
-  </div>
-  <router-view />
+          <li><router-link class="text-gray-100" to="/">Home</router-link></li>
+          <li
+            ><router-link class="text-gray-100" to="/about"
+              >About me</router-link
+            ></li
+          >
+          <li
+            ><router-link class="text-gray-100" to="/projects"
+              >Projects</router-link
+            ></li
+          >
+          <li
+            ><router-link class="text-gray-100" to="/Contact"
+              >Contact Me</router-link
+            ></li
+          >
+        </ul>
+      </nav>
+    </div>
+    <transition name="scale-slide">
+      <component :is="Component" :key="$route.path"> </component>
+    </transition>
+  </router-view>
 </template>
 <script>
 import { ref } from "vue";
@@ -61,3 +65,23 @@ export default {
   },
 };
 </script>
+
+<style lang="css">
+.scale-slide-enter-active,
+.scale-slide-leave-active {
+  position: absolute;
+  transition: all 0.85s ease;
+}
+.scale-slide-enter-from {
+  left: -100%;
+}
+.scale-slide-enter-to {
+  left: 0%;
+}
+.scale-slide-leave-from {
+  transform: scale(1);
+}
+.scale-slide-leave-to {
+  transform: scale(0.8);
+}
+</style>
