11aadc578e7a938d5fbfc1194cf95c55efeb2927
Sakhile Simelane <51701132+sakhike1@users.noreply.github.com>
2022-07-09 01:56:47 +0200

    Update HomeComponentTwo.vue

--- src/components/Home/HomeComponentTwo.vue
+++ src/components/Home/HomeComponentTwo.vue
@@ -2,28 +2,53 @@
   <div class="bg-black px-4 py-32 pb-6 sm:pb-8 lg:pb-12">
     <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
       <section
-        class="min-h-96 hover:bg-sky-700 flex justify-center items-center flex-1 shrink-0 bg-gray-100 overflow-hidden shadow-lg rounded-lg relative py-16 md:py-20 xl:py-48">
-        <img src="https://images.unsplash.com/photo-1618004652321-13a63e576b80?auto=format&q=75&fit=crop&w=1500"
-          loading="lazy" alt="Photo by Fakurian Design"
-          class="w-full h-full object-cover object-center absolute inset-0" />
+        class="min-h-96 hover:bg-sky-700 flex justify-center items-center flex-1 shrink-0 bg-gray-100 overflow-hidden shadow-lg rounded-lg relative py-16 md:py-20 xl:py-48"
+      >
+        <img
+          src="https://images.unsplash.com/photo-1618004652321-13a63e576b80?auto=format&q=75&fit=crop&w=1500"
+          loading="lazy"
+          alt="Photo by Fakurian Design"
+          class="w-full h-full object-cover object-center absolute inset-0"
+        />
         <div
-          class="bg-gradient-to-tl from-violet-900 via-rose-700 to-amber-400 hover:from-blue-500 hover:to-red-500 absolute inset-0">
+          class="bg-gradient-to-tl from-blue-700 via-blue-400 to-amber-100 hover:from-blue-200 hover:to-blue-500 absolute inset-0"
+        >
         </div>
         <div class="sm:max-w-xl flex flex-col items-center relative p-4">
-          <p class="text-indigo-200 text-lg sm:text-xl text-center mb-4 md:mb-8">
+          <p
+            class="text-indigo-200 text-lg sm:text-xl text-center mb-4 md:mb-8"
+          >
             Would you like to see
           </p>
-          <h1 class="text-white text-4xl sm:text-5xl md:text-6xl font-bold text-center mb-8 md:mb-12">
+          <h1
+            class="text-white text-4xl sm:text-5xl md:text-6xl font-bold text-center mb-8 md:mb-12"
+          >
             my coolest latest project ?
           </h1>
 
-          <div class="w-full flex flex-col sm:flex-row sm:justify-center gap-2.5">
-            <a href="https://environmental-woad.vercel.app/"
-              class="transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 duration-300inline-block hover:bg-indigo-600 active:bg-indigo-700 focus-visible:ring ring-indigo-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none px-8 py-3">Click
-              here</a>
+          <div
+            class="w-full flex flex-col sm:flex-row sm:justify-center gap-2.5"
+          >
+            <a
+              v-on:click="handleClick"
+              href="https://environmental-woad.vercel.app/"
+              class="transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 duration-300inline-block hover:bg-indigo-600 active:bg-indigo-700 focus-visible:ring ring-indigo-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none px-8 py-3"
+              >Click here</a
+            >
           </div>
         </div>
       </section>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: "HomeComponentTwo",
+  methods: {
+    handleClick() {
+      console.log("i've been clicked");
+    },
+  },
+};
+</script>
