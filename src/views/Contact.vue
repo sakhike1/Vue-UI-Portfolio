b6e4d0f0b7a9a829d00b843865b77146de4cd7d3
Sakhile Simelane <51701132+sakhike1@users.noreply.github.com>
2022-07-09 02:41:13 +0200

    Update Contact.vue

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="Contact">
     <Contactpage msg="Welcome to Your Vue.js App" />
-    <FooterComponent msg="Welcome to Your Vue.js App" />
+   
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Contactpage from "@/components/Contact/Contactpage.vue";
-import FooterComponent from "@/components/Shared/FooterComponent";
+
 
 export default {
   name: "Contact",
   components: {
     Contactpage,
-    FooterComponent,
+    
   },
 };
 </script>
