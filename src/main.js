bcb6409ed0c6b468a65e88e9b93cdc6e9bd9f2d7
Sakhile Simelane <51701132+sakhike1@users.noreply.github.com>
2022-07-10 11:47:54 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import { createApp } from 'vue'
+import VWave from 'v-wave'
 import App from './App.vue'
 import router from './router'
 import "./assets/tailwind.css";
-import './assets/tailwind.css'
+import './assets/tailwind.css';
 
-createApp(App).use(router).mount('#app')
+
+
+
+
+
+
+createApp(App).use(VWave).use(router).mount('#app')
