da01c0e57b3aabd0c254401c2b89aa25c9a55542
Sakhile Simelane <51701132+sakhike1@users.noreply.github.com>
2022-07-10 11:50:48 +0200

    Update HomeComponentThree.vue

--- src/components/Home/HomeComponentThree.vue
+++ src/components/Home/HomeComponentThree.vue
@@ -1,42 +1,25 @@
-<template>
-  <div
-    class="px-4 py-10 hover:from-blue-100 hover:to-red-100 bg-gradient-to-br from-blue-50 to-blue-100"
-  >
+<template data-aos="fade-down">
+  <div class="px-4 py-10 hover:from-blue-100 hover:to-red-100 bg-gradient-to-br from-blue-50 to-blue-100 ">
     <div class="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
-      <div
-        class="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12"
-      >
+      <div class="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
         <div class="space-y-6 md:7/12 lg:w-6/12">
-          <h2 class="text-2xl text-black-900 font-bold md:text-5xl"
-            >let your next development project be carried out by a passionate
-            developer.</h2
-          >
+          <h2 class="text-2xl text-black-900 font-bold md:text-5xl">let your next development project be carried out by
+            a passionate
+            developer.</h2>
           <div class="space-y-4">
-            <p class="text-gray-600"
-              >As a web developer, I enjoy using my obsessive attention to
+            <p class="text-gray-600">As a web developer, I enjoy using my obsessive attention to
               detail, my unequivocal love for making things, and my
-              mission-driven work ethic to literally change the world.</p
-            >
+              mission-driven work ethic to literally change the world.</p>
           </div>
-          <a
-            href="/#Contact"
-            class="block w-full py-3 px-6 text-center rounded-full transition bg-blue-200 hover:bg-blue-100 active:bg-blue-300 focus:bg-blue-100 sm:w-max"
-          >
+          <a href="/#Contact"
+            class="block w-full py-3 px-6 text-center rounded-full transition bg-blue-200 hover:bg-blue-100 active:bg-blue-300 focus:bg-blue-100 sm:w-max">
             <span class="block text-blue-900 font-semibold text-sm">
               Learn More
             </span>
           </a>
         </div>
-        <div
-          class="md:5/12 hover:scale-110 transition duration-300 ease-in-out lg:w-6/12"
-        >
-          <img
-            src="@/assets/Saly-36.png"
-            alt="image"
-            loading="lazy"
-            width=""
-            height=""
-          />
+        <div class="md:5/12 hover:scale-110 transition duration-300 ease-in-out lg:w-6/12">
+          <img src="@/assets/Saly-36.png" alt="image" loading="lazy" width="" height="" />
         </div>
       </div>
     </div>
@@ -47,4 +30,5 @@
 export default {};
 </script>
 
-<style></style>
+<style>
+</style>
