10764eecd3444395055020b78dd2e084e0b250e7
Sakhile Simelane <51701132+sakhike1@users.noreply.github.com>
2022-07-10 11:51:38 +0200

    Update HomeComponentTwo.vue

--- src/components/Home/HomeComponentTwo.vue
+++ src/components/Home/HomeComponentTwo.vue
@@ -5,6 +5,7 @@
         class="min-h-96 hover:bg-sky-700 flex justify-center items-center flex-1 shrink-0 bg-gray-100 overflow-hidden shadow-lg rounded-lg relative py-16 md:py-20 xl:py-48"
       >
         <img
+          
           src="https://images.unsplash.com/photo-1618004652321-13a63e576b80?auto=format&q=75&fit=crop&w=1500"
           loading="lazy"
           alt="Photo by Fakurian Design"
