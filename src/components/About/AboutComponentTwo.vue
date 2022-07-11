56a935b8bf9296484a800a099a27d41821ee9c6b
Sakhile Simelane <51701132+sakhike1@users.noreply.github.com>
2022-07-11 15:41:30 +0200

    Update AboutComponentTwo.vue

--- src/components/About/AboutComponentTwo.vue
+++ src/components/About/AboutComponentTwo.vue
@@ -1,19 +1,27 @@
 <template class="bg-black">
-  <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full  md:px-24 lg:px-8 lg:py-20 bg-black">
+  <div
+    class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full md:px-24 lg:px-8 lg:py-20 bg-black"
+  >
     <div class="grid gap-5 row-gap-10 lg:grid-cols-2">
-      <div class="flex flex-col justify-center ">
-        <div class="max-w-xl mb-6 ">
+      <div class="flex flex-col justify-center">
+        <div class="max-w-xl mb-6">
           <h2
-            class="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-indigo-600 sm:text-4xl sm:leading-none">
+            class="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-indigo-600 sm:text-4xl sm:leading-none"
+          >
             Check out, some of my <br class="hidden md:block" />
             skills
             <span class="relative px-1">
-              <div class="absolute inset-x-0 bottom-0 h-3 transform -skew-x-12 bg-teal-accent-400"></div>
+              <div
+                class="absolute inset-x-0 bottom-0 h-3 transform -skew-x-12 bg-teal-accent-400"
+              ></div>
             </span>
           </h2>
-          <p class="text-base text-neutral-600 max-w-xl mx-auto mt-8 leading-relaxed md:text-lg">
-            Vue.js, Angular, Javascript, Tailwind, HTML,laravel, CSS, SCSS, UXD, Figma,
-            Git, Vscode, macOS, windows, Vercel, Netlify, firebase,Vueex, bootstrap,Vuetify,Next.js
+          <p
+            class="text-base text-neutral-600 max-w-xl mx-auto mt-8 leading-relaxed"
+          >
+            Vue.js, Angular, Javascript, Tailwind, HTML,laravel, CSS, SCSS, UXD,
+            Figma, Git, Vscode, macOS, windows, Vercel, Netlify, firebase,Vueex,
+            bootstrap,Vuetify,Next.js
           </p>
         </div>
 
@@ -22,7 +30,9 @@
       <div>
         <img
           class="object-fit rounded-lg w-full h-56 hover:scale-90 transition duration-300 ease-in-out shadow-lg sm:h-96 bg-indigo-600"
-          src="@/assets/Saly-5.png" alt="" />
+          src="@/assets/Saly-5.png"
+          alt=""
+        />
       </div>
     </div>
   </div>
@@ -30,5 +40,4 @@
 
 <script></script>
 
-<style>
-</style>
+<style></style>
